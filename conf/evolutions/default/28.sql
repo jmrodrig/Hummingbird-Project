935b25eafcbd96f8f1d96618ed3c0346b9e635b4
jmrodrig <jose.neves.rodrigues@gmail.com>
2016-03-13 01:13:52 +0000

    last database correction fix

--- conf/evolutions/default/28.sql
+++ conf/evolutions/default/28.sql
@@ -1,11 +1,11 @@
 # --- !Ups
 
 ALTER TABLE `lir_backoffice`.`stories`
-CHANGE COLUMN `article_link` `article_link` LONGTEXT NULL DEFAULT NULL ;
+CHANGE COLUMN `article_link` `article_link` LONGTEXT NULL DEFAULT NULL ,
 CHANGE COLUMN `article_image` `article_image` LONGTEXT NULL DEFAULT NULL ;
 
 # --- !Downs
 
 ALTER TABLE `lir_backoffice`.`stories`
-CHANGE COLUMN `article_link` `article_link` VARCHAR(555) NULL DEFAULT NULL ;
+CHANGE COLUMN `article_link` `article_link` VARCHAR(555) NULL DEFAULT NULL ,
 CHANGE COLUMN `article_image` `article_image` VARCHAR(255) NULL DEFAULT NULL ;
