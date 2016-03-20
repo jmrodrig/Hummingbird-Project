da18d96155519d0a36a951c58ee3606363e1b1cd
jmrodrig <jose.neves.rodrigues@gmail.com>
2016-03-20 22:52:57 +0000

    database scheme for labels fix

--- conf/evolutions/default/29.sql
+++ conf/evolutions/default/29.sql
@@ -1,22 +1,16 @@
 # --- !Ups
 
 ALTER TABLE `lir_backoffice`.`stories_labels`
-ADD COLUMN `version` INT(11) NOT NULL AFTER `label_id`;
-DROP FOREIGN KEY `fk_stories_labels_labels_02`,
-DROP FOREIGN KEY `fk_stories_labels_stories_01`;
-ALTER TABLE `lir_backoffice`.`stories_labels`
-CHANGE COLUMN `story_id` `story_id` BIGINT(20) NULL ,
-CHANGE COLUMN `label_id` `label_id` BIGINT(20) NULL ,
-ADD COLUMN `id` BIGINT(20) NOT NULL AUTO_INCREMENT FIRST,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`id`);
-ALTER TABLE `lir_backoffice`.`stories_labels`
-ADD CONSTRAINT `fk_stories_labels_labels_02`
-  FOREIGN KEY (`label_id`)
-  REFERENCES `lir_backoffice`.`labels` (`id`),
-ADD CONSTRAINT `fk_stories_labels_stories_01`
-  FOREIGN KEY (`story_id`)
-  REFERENCES `lir_backoffice`.`stories` (`id`);
+  DROP FOREIGN KEY `fk_stories_labels_stories_01`,
+  DROP FOREIGN KEY `fk_stories_labels_labels_02`;
+  ALTER TABLE `lir_backoffice`.`stories_labels`
+  CHANGE COLUMN `story_id` `story_id` BIGINT(20) NULL ,
+  CHANGE COLUMN `label_id` `label_id` BIGINT(20) NULL ,
+  ADD COLUMN `id` BIGINT(20) NOT NULL AUTO_INCREMENT FIRST,
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (`id`),
+  DROP INDEX `fk_stories_labels_labels_02` ;
+
 
 # --- !Downs
 
