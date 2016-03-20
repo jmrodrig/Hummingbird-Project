d931cd508ccfa06650eee89c5e19a030f6efe637
jmrodrig <jose.neves.rodrigues@gmail.com>
2016-03-20 11:40:11 +0000

    Highlight hash tags on index create story view

--- public/javascripts/controls/index-view-handlers.js
+++ public/javascripts/controls/index-view-handlers.js
@@ -325,6 +325,7 @@ function openCreateStoryView() {
 	var largeStoryContainer = buildStoryLargeContainer(story,{editable:true,new:true});
 	$('#story-large-layout').animate({top: '100%'}, 150, "easeOutQuart", function() {
 		$('#story-large-layout-container').html(largeStoryContainer);
+		loadStoryTextBehaviours($('.story-summary'));
 		$('#map-viewport').innerWidth(contentwidth - $('#story-large-layout').outerWidth());
 		$('#story-large-layout-controllers').hide();
 		$('#story-large-layout').animate({top: 0}, 150, "easeOutQuart", function() {
@@ -354,6 +355,7 @@ function openEditStoryView(story) {
 	$('.location.editable').val(locationName);
 	var largeStoryContainer = buildStoryLargeContainer(story,{editable:true});
 	$('#story-large-layout-container').html(largeStoryContainer);
+	loadStoryTextBehaviours($('.story-summary'));
 	$('#map-viewport').innerWidth(contentwidth - $('#story-large-layout').outerWidth());
 	previousCenter = map.getCenter();
 	previousZoom = map.getZoom();
@@ -718,7 +720,6 @@ function buildStoryLargeContainer(story,options) {
 
 	if (options.editable) {
 		summary.attr('contenteditable', 'true').addClass('editable');
-		loadStoryTextBehaviours(summary);
 	}
 
   //Thumbnail
@@ -1899,7 +1900,8 @@ function formatArticleSource(art) {
 }
 
 function loadStoryTextBehaviours(element) {
-  var storyTextElem = element;
+  var storyTextElem = element,
+	caretPos;
   element.attr('contenteditable', 'true')
 
   // elastic behaviour
@@ -1921,7 +1923,121 @@ function loadStoryTextBehaviours(element) {
                 .focusout(function() {
                   if (storyTextElem.html() == '' || storyTextElem.html()== '<br>')
                     storyTextElem.html('<span class="placeholder">' + placeholder + '</span>').addClass('empty');
-                  });
+                })
+								.keyup(function() {
+									var _this = $(this)[0];
+									caretPos = getCaretPosition(_this);
+									console.log(caretPos.nodeIndex + ',' + caretPos.offset);
+									var innerHTML = '';
+									for (var i in _this.childNodes) {
+										if (_this.childNodes[i].nodeName == "#text") {
+											var textnode = _this.childNodes[i].data.replace(/(^|\s)([#][a-z\d-]+)/ig, "$1<span class='hash-tag'>$2</span>")
+											innerHTML = innerHTML + textnode;
+											if (textnode != _this.childNodes[i].data) {
+												caretPos.nodeIndex = caretPos.nodeIndex + 2;
+												caretPos.offset = 0;
+											}
+										} else if (_this.childNodes[i].nodeName == "SPAN") {
+											var textnode = _this.childNodes[i].innerHTML
+											if (textnode.indexOf('&nbsp;') > -1) {
+												innerHTML = innerHTML + "<span class='hash-tag'>" + textnode.replace('&nbsp;','') + "</span>&nbsp;";
+												caretPos.nodeIndex=caretPos.nodeIndex+2;
+												caretPos.offset = 0;
+											} else if (textnode.indexOf(' ') > -1) {
+												innerHTML = innerHTML + "<span class='hash-tag'>" + textnode.split(' ',2)[0] + "</span> " + textnode.split(' ',2)[1] ;
+												caretPos.nodeIndex++;
+												caretPos.offset = 0;
+											} else if (textnode.indexOf('#') != 0) {
+												innerHTML = innerHTML + textnode;
+												caretPos.nodeIndex++;
+												caretPos.offset = 0;
+											} else {
+												innerHTML = innerHTML + "<span class='hash-tag'>" + textnode + "</span>";
+											}
+										} else if (_this.childNodes[i].nodeName == "BR") {
+											innerHTML = innerHTML + '<br>';
+										} else if (_this.childNodes[i].nodeName == "DIV") {
+											var textnode = _this.childNodes[i].innerHTML.replace(/<font color="#\w+"><br><\/font>/ig,'<br>')
+																																	.replace('<span class="hash-tag">'," ").replace('</span>'," ")
+																																	.replace(/(^|\s)([#][a-z\d-]+)/ig, "$1<span class='hash-tag'>$2</span>");
+											if (_this.childNodes[i].previousSibling && _this.childNodes[i].previousSibling.nodeName != "BR" && _this.childNodes[i].previousSibling.nodeName != "DIV")
+												innerHTML = innerHTML + '<br>' + textnode;
+											else
+												innerHTML = innerHTML + textnode;
+										}
+									}
+									_this.innerHTML = innerHTML;
+									var currentRange = setCaretToPos(caretPos,_this)
+									_this.focus();
+
+									if (currentRange.startContainer.parentNode.nodeName == "SPAN") {
+										var bounding = currentRange.startContainer.parentNode.getBoundingClientRect();
+										$('<ul class="tag-list"/>').appendTo($('.lg-container .story-container-body'))
+																							.css({
+																								top:bounding.top + bounding.height - $(window).innerHeight() + storyContainersWrapperHeight,
+																								left:bounding.left
+																							});
+
+									} else {
+										$('.tag-list').remove();
+									}
+                });
+}
+
+function getCaretPosition(element) {
+  var caretOffset = 0,
+	nodeIndex = 0;
+
+  if (window.getSelection) {
+    var selection = window.getSelection(),
+    range = selection.getRangeAt(0),
+		caretNode = selection.anchorNode;
+    caretOffset = range.endOffset;
+		if (caretNode.parentNode.nodeName == "DIV" && caretNode.parentNode != element && caretNode != element) {
+			var child = caretNode.parentNode;
+			nodeIndex++;
+		} else if (caretNode.nodeName == "DIV" && caretNode != element) {
+			var child = caretNode;
+			nodeIndex++;
+		} else if (caretNode == element) {
+			return {offset:0,nodeIndex:selection.anchorOffset}
+		} else if (caretNode.parentNode.nodeName == "SPAN" && caretNode.parentNode.parentNode != element) {
+			var child = caretNode.parentNode.parentNode;
+			nodeIndex++;
+			caretOffset = 0;
+		} else if (caretNode.parentNode.nodeName == "SPAN") {
+			var child = caretNode.parentNode;
+			// if (caretOffset == caretNode.length) {
+			// 	nodeIndex++;
+			// 	caretOffset = 0;
+			// }
+		} else {
+			var child = caretNode;
+		}
+		while( (child = child.previousSibling) != null )
+		  nodeIndex++;
+	}
+
+  return {offset:caretOffset,nodeIndex:nodeIndex}
+}
+
+function setCaretToPos(pos,element) {
+	if (window.getSelection) {
+    var selection = window.getSelection()
+    range = document.createRange();
+		if (element.childNodes.length == 0)
+    	range.setStart(element,0);
+		else if (pos.nodeIndex >= element.childNodes.length)
+    	range.setStartAfter(element.childNodes[element.childNodes.length-1]);
+		else if (element.childNodes[pos.nodeIndex].nodeName == "SPAN")
+			range.setStart(element.childNodes[pos.nodeIndex].firstChild,pos.offset);
+		else
+			range.setStart(element.childNodes[pos.nodeIndex],pos.offset);
+		range.collapse(true);
+		selection.removeAllRanges();
+		selection.addRange(range);
+		return range;
+	}
 }
 
 function unloadStoryTextBehaviours(element) {
