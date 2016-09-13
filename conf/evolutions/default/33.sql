84f4fdf2e9a56457702d80669e5513a35779d980
jmrodrig <jose.neves.rodrigues@gmail.com>
2016-09-13 19:21:13 +0100

    NEW VERSION ::: LIR 2.0 db evolution correction

--- conf/evolutions/default/33.sql
+++ conf/evolutions/default/33.sql
@@ -3,7 +3,7 @@
 ALTER TABLE `lir_backoffice`.`stories`
 CHANGE COLUMN `story_language` `language` VARCHAR(45) NULL DEFAULT NULL ,
 ADD COLUMN `format` TINYINT(1) NULL DEFAULT NULL AFTER `date_created`,
-ADD COLUMN `premium` TINYINT(1) NULL DEFAULT NULL AFTER `format`;
+ADD COLUMN `premium` TINYINT(1) NULL DEFAULT NULL AFTER `format`,
 ADD COLUMN `date_modified` DATETIME NULL DEFAULT NULL AFTER `premium`;
 
 
@@ -12,5 +12,5 @@ ADD COLUMN `date_modified` DATETIME NULL DEFAULT NULL AFTER `premium`;
 ALTER TABLE `lir_backoffice`.`stories`
 CHANGE COLUMN `language` `story_language` VARCHAR(45) NULL DEFAULT NULL ,
 DROP COLUMN `format`,
-DROP COLUMN `premium`
+DROP COLUMN `premium`,
 DROP COLUMN `date_modified`;
